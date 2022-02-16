9876477c86aedf23f8c942353d3d2d3b60e41c90
dinton harold <dintonharold@gmail.com>
2022-02-16 16:11:40 +0300

    Added styling to the navbar and main content

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,55 @@
-import React from 'react';
-export default function Main(){
-    return(
-        <h1>Main section goes here</h1>
-    )
+import React from "react";
+import './Main.css';
+export default function Main() {
+  return (
+    <div className="frame-12">
+      <div className="frame-11">
+        <h1 className="h1-titleinter-bold-white-39-1px">
+          <span className="inter-bold-white-39-1px">Fun facts about React</span>
+        </h1>
+        <div className="frame-10">
+          <div className="frame-5">
+            <div className="ellipse-1"></div>
+            <p className="was-first-released-in-2013inter-normal-white-16px">
+              <span className="inter-normal-white-16px">
+                Was first released in 2013
+              </span>
+            </p>
+          </div>
+          <div className="frame-6">
+            <div className="ellipse-1"></div>
+            <p className="was-originally-created-by-jordan-walkeinter-normal-white-16px">
+              <span className="inter-normal-white-16px">
+                Was originally created by Jordan Walke
+              </span>
+            </p>
+          </div>
+          <div className="frame-7">
+            <div className="ellipse-1"></div>
+            <p className="has-well-over-100-k-stars-on-git-hubinter-normal-white-16px">
+              <span className="inter-normal-white-16px">
+                Has well over 100K stars on GitHub
+              </span>
+            </p>
+          </div>
+          <div className="frame-8">
+            <div className="ellipse-1"></div>
+            <div className="is-maintained-by-facebookinter-normal-white-16px">
+              <span className="inter-normal-white-16px">
+                Is maintained by Facebook
+              </span>
+            </div>
+          </div>
+          <div className="frame-9">
+            <div className="ellipse-1"></div>
+            <p className="surnameinter-normal-white-16px">
+              <span className="inter-normal-white-16px">
+                Powers thousands of enterprise apps, including mobile apps
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,21 @@
 import React from 'react';
-export default function Navbar(){
-    return(
-        <h1>Navbar goes here</h1>
-    )
+import './Navbar.css';
+// import '../../public/style.css'
+import logo from "./reactjs-icon-1@2x.png";
+export default function Navbar() {
+  return (
+    <div className="frame-4">
+      <div className="frame-3">
+        <img className="reactjs-icon" src={logo} alt="Logo" />
+        <div className="react-factsinter-bold-turquoise-blue-22-3px">
+          <span className="inter-bold-turquoise-blue-22-3px">ReactFacts</span>
+        </div>
+      </div>
+      <p className="extra-titleinter-normal-link-water-16px">
+        <span className="inter-normal-link-water-16px">
+          React Course - Project 1
+        </span>
+      </p>
+    </div>
+  );
 }
